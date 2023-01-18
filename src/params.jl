2cbc8d8bc2c9233b711b68753099f682dd48511c
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-01-18 14:10:29 +0100

    Add doc strings

--- src/params.jl
+++ src/params.jl
@@ -300,7 +300,60 @@ scparams_regularize(params::NamedTuple, bw) = scparams_regularize(NamedTuple,par
 
 
 
+"""
+	scparams([T], X::AbstractSparseMatrix, features;
+	         method=:poisson,
+	         min_cells::Integer=5,
+	         feature_type,
+	         feature_mask,
+	         feature_names,
+	         use_cache=true,
+	         cache_read=use_cache,
+	         cache_write=use_cache,
+	         verbose=true,
+	         chunk_size=100,
+	         nthreads=Threads.nthreads(),
+	         channel_size=nthreads*4)
 
+Compute SCTransform parameter estimates from the count matrix `X`, with features as rows and cells as columns.
+`features` should be a table (e.g. DataFrame or NamedTuple) with feature annotations.
+`T` is an optional parameter for the type of the output parameter table. It defaults to the same type as `features`.
+By default, the results are cached in a scratch space using `Scratch.jl`, to avoid unnecessary recomputations across julia sessions.
+
+General:
+* `method` - Decides the algorithm for parameter inference. Can be either `:poisson` or `:nb` (negative binomial). We recommend `:poisson`, which first makes `poission` estimates and then estimates disperation afterwards.
+* `feature_names` - Vector with name for each feature. Only used for `@warn` messages. Defaults to `name` column in `features`, or `id` column if name doesn't exist.
+* `verbose` - If true, will show progress bar and some other information.
+
+Feature selection:
+* `min_cells` - Only features with nonzero counts in at least `min_cells` cells are included.
+* `feature_type` - Convenience parameter used for `feature_mask` default. Defaults to "Gene Expression" if `features` has a `feature_type` column.
+* `feature_mask` - Vector of booleans deciding which features to use. If set explicitly, the `feature_type` parameter is ignored, otherwise `feature_mask` defaults to only choosing the `feature_type` specified.
+
+Cache:
+* `use_cache` - Enable/disabling caching.
+* `cache_read` - More fine-grained control to enable/disable cache reading only.
+* `cache_write` - More fine-grained control to enable/disable cache writing only.
+
+Threading details:
+* `chunk_size` - The number of features to process in one chunk.
+* `nthreads` - How many threads to use from parameter estimation.
+* `channel_size` - How many chunks to keep in the queue.
+
+
+# Examples
+Compute the SCTransform with default parameters (Gene Expression data):
+```
+julia> scparams(X, features)
+```
+
+Compute the SCTransform for antibody capture features:
+```
+julia> scparams(X, features; feature_type="Antibody Capture")
+```
+
+See also: [`sctransform`](@ref)
+"""
 function scparams(::Type{T}, X::AbstractSparseMatrix, features;
                   method=:poisson,
                   min_cells::Integer=5,
--- src/transform.jl
+++ src/transform.jl
@@ -1,5 +1,22 @@
-# assumes each column is a cell
-function sctransform(X::SparseMatrixCSC, features, params;
+"""
+	sctransform(X::AbstractSparseMatrix, features, params;
+                transpose = false,
+                feature_id_columns = [:id,:feature_type],
+                cell_ind = 1:size(X,2),
+                clip=sqrt(size(X,2)/30))
+
+Computes the SCTransform of the sparse matrix `X`, with features as rows and cells as columns.
+`features` should be a table (e.g. DataFrame or NamedTuple) with feature annotations.
+`params` is a table with reguralized feature parameter estimates - typically estimated using the function `scparams`.
+
+* `transpose` - set to true to transpose the output (cells as rows, features as columns).
+* `feature_id_columns` is a vector of column names in `features`. The rows in `features` must be unique based on these columns.
+* `cell_ind` is vector of cell indices to include in the output. This is computationally more efficient than subsetting afterwards, but yields the same result.
+* `clip` - values less than `-clip` or larger than `clip` are clamped, to reduce the impact of outliers.
+
+See also: [`scparams`](@ref)
+"""
+function sctransform(X::AbstractSparseMatrix, features, params;
                      transpose = false,
                      feature_id_columns = [:id,:feature_type],
                      cell_ind = 1:size(X,2),
